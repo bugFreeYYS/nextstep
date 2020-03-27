e10074816f1bb2f7e399e6debdc1f862fa9d9591
YangYuesong <E0015005@u.nus.edu>
2020-03-27 16:17:04 +0800

    7

--- setup.py
+++ setup.py
@@ -5,19 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nextstep",
-    version="0.0.2",
+    version="0.0.7",
     author="Yang Yuesong",
     author_email="yangyuesongyys@gmail.com",
     description="USEP price prediction",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/YangYuesong0323/EMC",
+    url="https://github.com/YangYuesong0323/nextstep",
     packages=setuptools.find_packages(),
-    python_requires='>=3.6',
-    install_requires = [
-        'dataflows == 0.071',
-        'wwoh-hist == 0.0.4',
-        'tenserflow == 1.1.0',
-        'statsmodels == 0.10.1'
-        ]
+    python_requires='>=3.6'
 )
