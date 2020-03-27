36be2b59c908dcea3933a6e86a8a0fd54d471c06
YangYuesong <E0015005@u.nus.edu>
2020-03-27 15:48:39 +0800

    v2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nextstep",
-    version="0.0.1",
+    version="0.0.2",
     author="Yang Yuesong",
     author_email="yangyuesongyys@gmail.com",
     description="USEP price prediction",
